9d24112e8edf838b5db24a3e1fc2c93eda795165
Jose Saul Giffuni Becerra <giffunis@gmail.com>
2015-11-02 17:48:08 +0000

    Creado el Frame y el JDesktopPane

--- src/ImageEditor2/ImageEditor2.java
+++ src/ImageEditor2/ImageEditor2.java
@@ -1,5 +1,49 @@
 package ImageEditor2;
 
-public class ImageEditor2 {
+import java.awt.*;
+import javax.swing.*;
 
+public class ImageEditor2 {
+	JFrame framePrincipal;
+	JDesktopPane desktopPane;
+	//JMenuBar2 menuBar;
+	//JToolBar2 toolBar2;
+	
+	ImageEditor2(){
+		
+		initFramePrincipal();
+		initDesktopPane();
+//		initToolBar2();
+//		initMenuBar2();
+		
+//		framePrincipal.add("North", barraBotonesPrincipal);	//	Añadimos barraPrincipal al framePrincipal
+//		framePrincipal.setJMenuBar(menuBar);
+
+	}
+	
+	void initFramePrincipal(){
+		framePrincipal = new JFrame("ImageEditor2");
+		framePrincipal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		framePrincipal.setLayout(new BorderLayout());
+		framePrincipal.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		framePrincipal.setVisible(true);
+	}
+	
+	void initDesktopPane(){
+		desktopPane = new JDesktopPane();
+		this.framePrincipal.add(desktopPane,BorderLayout.CENTER);
+	}
+	
+	void initToolBar2(){
+		
+	}
+	
+	void initMenuBar2(){
+		
+	}
+	
+	public static void main(String[] args) {
+    	@SuppressWarnings("unused")
+		ImageEditor2 programa = new ImageEditor2();
+    }
 }
